cc287f6b211ba6ca77a97063a49f501d0e7168ea
Dunya Kirkali <dunyakirkali@gmail.com>
2017-08-06 13:15:53 +0200

    Try camera

--- festicam/ViewController.swift
+++ festicam/ViewController.swift
@@ -10,16 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var wView: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+//        let url = Bundle.main.path(forResource: "index", ofType: "html")
+//        let contents: String = try! NSString(contentsOfFile: url!, encoding: String.Encoding.utf8.rawValue) as String
+//        let baseUrl: URL = NSURL(fileURLWithPath: url!) as URL
+        let str = String("https://tutorialzine.github.io/pwa-photobooth/")
+        let url = URL(string: str!)
+        let request = URLRequest(url: url!)
+        wView.loadRequest(request)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
-
